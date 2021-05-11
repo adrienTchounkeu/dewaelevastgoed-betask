297da10d373f117ac60acd9d025d9e08520e364f
Adrien Tchounkeu <adrientchounkeu10@gmail.com>
2021-05-11 17:28:30 +0100

    Perform Task 2 # add tests to perform Task 1

--- articles/tests/test_views.py
+++ articles/tests/test_views.py
@@ -28,6 +28,46 @@ class ArticleListCreateAPIViewTest(APITestCase):
         article = Article.objects.get(id=response.data["id"])  # newly-created
         self.assertEqual(article.slug, "test-article")
 
+    def test_filter(self):
+        article1, article2, article3 = ArticleFactory.create_batch(3)
+        # test filter with title
+        for article in [article1, article2, article3]:
+            response = self.client.get(reverse("article_list") + f"?title={article.title}")
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(
+                [article["id"] for article in response.data], [article.id]
+            )
+        # test filter with the content
+        response = self.client.get(reverse("article_list") + "?content=Lorem ipsum")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            [article["id"] for article in response.data], [article1.id, article2.id, article3.id]
+        )
+
+    def test_order(self):
+        article1, article2 = ArticleFactory.create_batch(2)
+
+        # test ordering in ascendant order with title
+        response = self.client.get(reverse("article_list") + f"?ordering=title")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            [article["id"] for article in response.data], [article1.id, article2.id]
+        )
+
+        # test ordering in descendant order with title
+        response = self.client.get(reverse("article_list") + f"?ordering=-title")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            [article["id"] for article in response.data], [article2.id, article1.id]
+        )
+
+        # test ordering with created_at
+        response = self.client.get(reverse("article_list") + f"?ordering=created_at")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            [article["id"] for article in response.data], [article1.id, article2.id]
+        )
+
 
 class ArticleDetailAPIViewTest(APITestCase):
     def test_update(self):
