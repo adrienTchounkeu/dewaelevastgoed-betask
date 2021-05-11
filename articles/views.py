791749d7112c30d04af64defdb55219f818d1781
Adrien Tchounkeu <adrientchounkeu10@gmail.com>
2021-05-11 16:39:01 +0100

    Perform task 1 # add django_filters modules # add filter by title and content # add to sort by title and created_at

--- articles/views.py
+++ articles/views.py
@@ -2,11 +2,16 @@ from rest_framework import generics
 
 from articles.models import Article
 from articles.serializers import ArticleSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 
 class ArticleListCreateAPIView(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ['title', 'content']
+    ordering_fields = ['title', 'created_at']
 
 
 class ArticleDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
